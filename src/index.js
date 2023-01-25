96b0fb86627cbb0673677f24b5189e95c9612a9c
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-25 20:41:39 -0300

    Create root route in Express server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 const express = require('express');
-const app = express();
 
+const app = express();
+const PORT = 3000;
+
+app.get('/', (request, response) => response.json({
+  message: 'Olá Dev! Bem vindo ao curso!',
+}));
+
+/**
+ * Para que uma aplicação cliente possa acessar a aplicação que estamos criando,
+ * precisamos disponibilizar um servidor e porta.
+ * Recomendação é utilizar portas acima de 1024, pois de 0 a 1023 elas são portas reservadas para
+ * serviços conhecidos.
+ */
+app.listen(PORT, () => console.log('🔥 Server running at http://localhost:3000'));
