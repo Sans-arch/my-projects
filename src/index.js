b9be31c160ebcbe4770864042f8c36acf5480d51
Santiago Fleitas <sansfleitas@gmail.com>
2023-01-25 21:06:53 -0300

    Receive route params

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ const PORT = 3000;
 
 app.get('/projects', (request, response) => {
   const { title, owner, page } = request.query;
-  console.log({
-    title, owner, page,
-  });
+  console.log({ title, owner, page });
 
   return response.json([
     'Projeto 1',
@@ -24,11 +22,16 @@ app.post('/projects', (request, response) => response.json([
   'Projeto 3',
 ]));
 
-app.put('/projects/:id', (request, response) => response.json([
-  'Projeto 4',
-  'Projeto 2',
-  'Projeto 3',
-]));
+app.put('/projects/:id/:name', (request, response) => {
+  const { id, name } = request.params;
+  console.log(id, name);
+
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
 
 app.delete('/projects/:id', (request, response) => response.json([
   'Projeto 2',
